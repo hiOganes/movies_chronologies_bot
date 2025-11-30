d5f46d3031d3427ef4d2c9a0df087b48f2e6d9d4
hiOganes <soft.oganes@gmail.com>
2025-11-30 17:48:29 +0300

    add inline keyboard

--- bot/keyboards/reply.py
+++ bot/keyboards/reply.py
@@ -3,8 +3,11 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 reply_menu = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text='Марвел')],
-        [KeyboardButton(text='Заклятие'), KeyboardButton(text='Властелин колец')],
+        [KeyboardButton(text='Марвел',)],
+        [
+            KeyboardButton(text='Заклятие'),
+            KeyboardButton(text='Властелин колец')
+        ],
         [KeyboardButton(text='Звёздные войны')]
     ],
     resize_keyboard=True,
